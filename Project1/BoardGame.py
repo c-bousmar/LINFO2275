43b0a5153b5cb0fff9193d651a288d341c6474fb
Mathis003 <mathis.delsart@student.uclouvain.be>
2025-02-21 21:54:24 +0100

    MarkovDecision Algo => OK

--- Project1/BoardGame.py
+++ Project1/BoardGame.py
@@ -15,6 +15,9 @@ class BoardGame:
         self.dice   = [ Die(type_die=DieType.SECURITY, moves=[0, 1], probabilities=[1/2, 1/2], probability_triggers=0.0),
                         Die(type_die=DieType.NORMAL, moves=[0, 1, 2], probabilities=[1/3, 1/3, 1/3], probability_triggers=1/2),
                         Die(type_die=DieType.RISKY, moves=[0, 1, 2, 3], probabilities=[1/4, 1/4, 1/4, 1/4], probability_triggers=1.0) ]
+        # self.dice   = [ Die(type_die=DieType.SECURITY, moves=[1], probabilities=[1], probability_triggers=1.0),
+        #                 Die(type_die=DieType.NORMAL, moves=[2], probabilities=[1], probability_triggers=1.0),
+        #                 Die(type_die=DieType.RISKY, moves=[3], probabilities=[1], probability_triggers=1.0) ]
     
     def display_board(self):
         layout = "Board :\n"
@@ -25,7 +28,6 @@ class BoardGame:
             range(PositionType.FAST_LANE_LAST_CELL.value + 1, PositionType.SLOW_LANE_LAST_CELL.value + 1),
             range(PositionType.FINAL_CELL.value, PositionType.FINAL_CELL.value + 1)
         ):
-            print(i)
             emoji = CellTypeEmoji[self.states[i].cell_type.name].value
             nb_whitespace = 2 if (emoji == "X" or emoji == "⬅") else 1
             layout += f"  {emoji}" + " " * nb_whitespace + "|"
--- Project1/markovDecision.py
+++ Project1/markovDecision.py
@@ -1,59 +1,77 @@
-from BoardGame import BoardGame
 from Enum import PositionType, CellType
+from BoardGame import BoardGame
 
 import numpy as np
 
 def transition_probabilities(state, die, board):
-    
     transitions = {}
+    offsets = [0, 8] if (state.position == PositionType.SLOW_LANE_FIRST_CELL.value - 1) else [0]
 
-    for idx_move, step_size_move in enumerate(die.moves):
-        
-        offsets = [0]
-        
-        # Handling the case where we have the fast lane and the slow lane
-        if state.position_type == PositionType.SLOW_LANE_FIRST_CELL.value - 1:
-            offsets.append(8)
+    for prob_move, step_size_move in zip(die.probabilities, die.moves):        
         
         for offset in offsets:
-            new_position = state.position + step_size_move + offset
+            # Get next position
+            new_position = get_next_position(state.position, step_size_move, offset, board)
 
-            # Handling circle (if any) and wrapping around the board
-            if new_position > PositionType.FINAL_CELL.value:
-                new_position = PositionType.FINAL_CELL.value if not board.circle else new_position - len(board.states)
-        
-            new_state = board.states[new_position]
-            
-            # Handling traps and special squares
-            extra_cost = 0
-            if new_state.cell_type == CellType.RESTART and die.is_triggering_event():
-                new_position = 0
-            elif new_state.cell_type == CellType.PENALTY and die.is_triggering_event():
-                new_position = max(0, new_position - 3)
-            elif new_state.cell_type == CellType.PRISON and die.is_triggering_event():
-                extra_cost = 1
-            elif new_state.cell_type == CellType.BONUS and die.is_triggering_event():
-                extra_cost = -1
+            # Case where the event is not triggered
+            current_prob, current_extra = transitions.get(new_position, (0, 0))
+            transitions[new_position] = (
+                current_prob + (prob_move * (1 - die.probability_triggers)) / len(offsets),
+                current_extra
+            )
 
-            prob, cost = transitions.get(new_position, (0, 0))
-            transitions[new_position] = (prob + die.probabilities[idx_move] / len(offsets), cost + extra_cost)
+            # Case where the event is triggered
+            new_position_trigger, extra_cost = handling_events(board.states[new_position])
+            current_prob, current_extra = transitions.get(new_position_trigger, (0, 0))
+            transitions[new_position_trigger] = (
+                current_prob + (prob_move * die.probability_triggers) / len(offsets),
+                current_extra + (prob_move * die.probability_triggers * extra_cost) / len(offsets)
+            )
 
     return transitions
 
-def get_cost_to_goal_state(position):
-    if PositionType.FAST_LANE_FIRST_CELL.value <= position <= PositionType.FAST_LANE_LAST_CELL.value:
-        return PositionType.FINAL_CELL.value - position
-    else:
-        return PositionType.SLOW_LANE_LAST_CELL.value - position + 1
+def handling_events(state):
+    match state.cell_type:
+        case CellType.RESTART:
+            return 0, 0
+        case CellType.PENALTY:
+            return max(0, state.position - 3), 0
+        case CellType.PRISON:
+            return state.position, 1
+        case CellType.BONUS:
+            return state.position, -1
+    return state.position, 0
+
+def get_next_position(position, step_size_move, offset, board):
+    # Basic movement
+    new_position = position + step_size_move + offset
+    
+    # Handle special cases
+    if offset == PositionType.FAST_LANE_FIRST_CELL.value - PositionType.SLOW_LANE_FIRST_CELL.value + 1:
+        new_position -= 1
+        if new_position == PositionType.SLOW_LANE_LAST_CELL.value:
+            new_position = position
+    
+    # Handling the gap between the 9_th position and the 14_th position (only one cell)
+    if PositionType.SLOW_LANE_FIRST_CELL.value <= position <= PositionType.SLOW_LANE_LAST_CELL.value:
+        if not (PositionType.SLOW_LANE_FIRST_CELL.value <= new_position <= PositionType.SLOW_LANE_LAST_CELL.value):
+            new_position += PositionType.FAST_LANE_LAST_CELL.value - PositionType.FAST_LANE_FIRST_CELL.value + 1
+
+    # Handling circle (if any) and wrapping around the board
+    if new_position > PositionType.FINAL_CELL.value:
+        new_position = PositionType.FINAL_CELL.value if not board.circle else new_position - len(board.states)
+    
+    return new_position
     
 def markovDecision(layout, circle):
     """
     Solves the Markov Decision Process using Value Iteration.
     """
-    max_iterations = 50000
+    max_iterations = 1000
     tol = 1e-9
     
     board = BoardGame(layout, circle)
+    board.display_board()
 
     V_hat_k = np.zeros(len(board.states), dtype=float)
     policies = np.zeros(len(board.states), dtype=int)
@@ -67,11 +85,10 @@ def markovDecision(layout, circle):
 
         for state in board.states[:-1]:
             V_k = {}
-            cost = get_cost_to_goal_state(state.position)
-
+            
             for die in board.dice:
                 transitions = transition_probabilities(state, die, board)
-                expected_cost = cost + sum(p * (V_hat_k[pos] + extra) for pos, (p, extra) in transitions.items())
+                expected_cost = 1.0 + sum(extra + p * V_hat_k[pos] for pos, (p, extra) in transitions.items())
                 V_k[die.type.value] = expected_cost
 
             best_action = min(V_k, key=V_k.get)
@@ -84,3 +101,41 @@ def markovDecision(layout, circle):
         iteration += 1
 
     return [V_hat_k[:-1], policies[:-1]]
+
+
+if __name__ == '__main__':
+    
+    # layout = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    # result = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  
+    # layout = [0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0]
+    # result = [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1]
+    
+    # layout = [0, 2, 0, 2, 0, 1, 0, 2, 0, 1, 0, 1, 0, 1, 0]
+    # result = [2, 1, 2, 3, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1]
+    
+    # layout = [0, 2, 0, 2, 0, 1, 0, 2, 0, 1, 4, 4, 4, 4, 0]
+    # result = [2, 1, 3, 3, 2, 3, 2, 1, 1, 1, 3, 3, 3, 3]
+    
+    # layout = [0, 2, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 2, 0]
+    # result = [3, 3, 2, 3, 3, 3, 2, 1, 1, 1, 1, 1, 1, 1]
+    
+    # layout = [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+    # result = [2, 1, 3, 3, 2, 1, 1, 1, 1, 1, 3, 3, 3, 3]
+    
+    # layout = [0, 1, 3, 4, 2, 0, 0, 0, 0, 1, 1, 2, 1, 2, 0]
+    # result = [3, 2, 1, 3, 3, 3, 2, 1, 1, 1, 1, 1, 1, 1]
+    
+    # layout = [0, 2, 3, 2, 0, 2, 2, 0, 1, 0, 0, 3, 1, 3, 0]
+    # result = [2, 1, 2, 2, 3, 1, 1, 1, 1, 3, 1, 1, 1, 3]
+    
+    # layout = [0, 0, 3, 1, 1, 3, 2, 2, 4, 0, 4, 4, 0, 0, 0]
+    # result = [2 1 1 1 1 1 1 1 3 3 1 1 3 3]
+    
+    layout = [0, 0, 3, 1, 1, 3, 2, 2, 4, 0, 4, 4, 0, 0, 0]
+    circle = True
+    expectations, die_optimal = markovDecision(layout, circle)
+    print(expectations)
+    # print(die_optimal)
+    print(die_optimal[:10])
+    print("     " + f'{die_optimal[10:15]}')
