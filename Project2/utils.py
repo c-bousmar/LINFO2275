3be6a5067ab246ed9fb21ebe053c466e5aa9ed22
Mathis003 <mathis.delsart@student.uclouvain.be>
2025-04-16 11:34:07 +0200

    Add Script .py to combine csv into a single dataset

---
+++ Project2/utils.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import csv
+
+def user_independant_score(model):
+    pass
+
+def user_dependant_score(model):
+    pass
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    data = pd.read_csv("Data/Datasets_CSV/Domain1_csv/Subject1-0-1.csv")
+    
